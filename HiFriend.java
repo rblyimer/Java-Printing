5e1a52949976311d329c0b76e8e8500831ec6dae
rblyimer <101184162+rblyimer@users.noreply.github.com>
2022-05-03 05:35:04 -0400

    first commit

---
+++ HiFriend.java
@@ -0,0 +1,8 @@
+public class HiFriend {
+    public static void main(String[] args) {
+      String s1 = "Hi";
+      String s2 = " " + "Robel" + " " + "Yimer";
+      String s3 = s1 + s2;
+      System.out.println(s3);   
+  }
+  }
---
+++ Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+    public static void main(String[] args) {
+      System.out.println("I would like to vist");
+      System.out.println("Venice");
+      System.out.println("Italy");
+  }
+  }
